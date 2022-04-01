e333b58bf6a7e32898c192880558499ecf57b4c3
cmendonca <carlos.mendonca@equilibriumweb.com>
2022-04-01 08:57:42 -0300

    Initial commit

---
+++ src/test/java/com/spring/guidesecurity/security/SecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.guidesecurity.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
